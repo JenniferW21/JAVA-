e675658a8fdc8d7270545929225c330b24983a47
JenniferW21 <jenniferwang570@gmail.com>
2025-09-29 16:32:14 +0000

    java

---
+++ Java-7-CW/Main.java
@@ -0,0 +1,34 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  void init(){
+  // For each problem below, prompt the user for the input values for each variable in the formula.
+    
+  /*
+    Problem 1: Translate the formula from eq1.png.
+  */
+  double z = 0;
+  double A = 5.0;
+  double x = 3.0;
+  z = Math.pow(A,2*x+1);
+  System.out.println("z = " + z);
+
+    
+  /*
+    Problem 2: Translate the formula from eq2.png.
+  */
+
+
+    
+    /*
+    Problem 3: Translate the formula from eq3.png.
+  */
+
+    
+
+  }
+
+  
+}
